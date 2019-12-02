acf2ba475556422be75b842888ffb1d7007c9958
Александр Медолиз <aleksandrmedoliz@MacBook-Pro-Aleksandr.local>
2019-12-02 14:03:34 +0200

    added fix task4

--- JS/hm1/js/task4.js
+++ JS/hm1/js/task4.js
@@ -4,12 +4,12 @@ function home4()
 {
     let credits = 23580;
     let pricePerDroid = 3000;
-    let totalPrice;
     let maseeage = prompt ("Какое количество дроидов нужно купить");
-    alert (maseeage);
+    let totalPrice = pricePerDroid * maseeage;
     if (maseeage === null) {
         alert ("Отменено пользователем!");
     } else if (credits > pricePerDroid) {
         console.log (pricePerDroid = totalPrice);
     } 
+    
 }
