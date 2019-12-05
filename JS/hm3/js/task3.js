d960691bc18ed15e8bd4637b51e3469d123aba44
Александр Медолиз <aleksandrmedoliz@MacBook-Pro-Aleksandr.local>
2019-12-05 15:09:50 +0200

    added task3

---
+++ JS/hm3/js/task3.js
@@ -0,0 +1,23 @@
+"use strict";
+function home3() {
+  let findBestEmployee = function(employees) {
+    let a = 0;
+    let b;
+    for (let key in employees) {
+      if (employees[key] > a) {
+        a = employees[key];
+        b = key;
+      }
+    }
+    console.log(`${b} - ${a}`);
+  };
+
+  console.log(
+    findBestEmployee({
+      ann: 29,
+      david: 35,
+      helen: 1,
+      lorence: 99
+    })
+  ); // lorence
+}
