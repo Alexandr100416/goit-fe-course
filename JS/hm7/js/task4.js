912b3dcbeb70926a4c107797704dc89350d98e3f
Александр Медолиз <aleksandrmedoliz@MacBook-Pro-Aleksandr.local>
2019-12-19 11:29:15 +0200

    added tas4

---
+++ JS/hm7/js/task4.js
@@ -0,0 +1,9 @@
+let counterValue = 0;
+function increment() {
+  counterValue += 1;
+  document.getElementById("value").innerHTML = counterValue;
+}
+function decrement() {
+  counterValue -= 1;
+  document.getElementById("value").innerHTML = counterValue;
+}
