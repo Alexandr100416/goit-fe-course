3c98f40f0b88b8d038353a568604e3c377cbf28f
Александр Медолиз <aleksandrmedoliz@MacBook-Pro-Aleksandr.local>
2019-12-22 23:34:19 +0200

    added task5

--- JS/hm7/js/task5.js
+++ JS/hm7/js/task5.js
@@ -1,1 +1,9 @@
-console.log("2esw")
+const textInput = document.getElementById("name-input");
+
+const textSpan = document.getElementById("name-output");
+
+const cumEl = function(elem) {
+  const field = elem.currentTarget;
+  textSpan.textContent = field.value !== "" ? field.value : "незнакомец";
+};
+textInput.addEventListener("input", cumEl.bind(textSpan));
