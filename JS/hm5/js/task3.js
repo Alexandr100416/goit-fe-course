6f8b14d871f5f91a3a30ab2a6338c5afd666afd9
Александр Медолиз <aleksandrmedoliz@MacBook-Pro-Aleksandr.local>
2020-05-07 18:28:07 +0300

    fixced task3

--- JS/hm5/js/task3.js
+++ JS/hm5/js/task3.js
@@ -2,37 +2,36 @@
 function home3() {
   class Storage {
     constructor(arr) {
-      this.arr=arr;
+      this.arr = arr;
     }
-    getItems(){
-      
+    getItems() {
       return this.arr;
     }
-    addItem(item){
-  this.arr.push(item);
-  console.table(this.arr);
+    addItem(item) {
+      this.arr.push(item);
+      console.table(this.arr);
     }
-    removeItem(item){
-      const findElem=this.arr.indexOf(item)
-      if(findElem>-1){
+    removeItem(item) {
+      const findElem = this.arr.indexOf(item);
+      if (findElem > -1) {
         this.arr.splice(findElem, 1);
       }
       console.table(this.arr);
     }
   }
   const storage = new Storage([
-    'Нанитоиды',
-    'Пролонгер',
-    'Железные жупи',
-    'Антигравитатор',
+    "Нанитоиды",
+    "Пролонгер",
+    "Железные жупи",
+    "Антигравитатор",
   ]);
-  
+
   const items = storage.getItems();
   console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
-  
-  storage.addItem('Дроид');
+
+  storage.addItem("Дроид");
   // console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
-  
-  storage.removeItem('Пролонгер');
+
+  storage.removeItem("Пролонгер");
   // console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
 }
