24be07360cc0371d68093904cda86f63315719d3
Александр Медолиз <aleksandrmedoliz@MacBook-Pro-Aleksandr.local>
2020-05-07 15:14:56 +0300

    added class variant task#1

--- JS/hm5/js/task1.js
+++ JS/hm5/js/task1.js
@@ -1,11 +1,11 @@
 "use strict";
 function home1() {
-  const Account = function({ login = "qwerty", email = "qwerty" }) {
+  const Account = function ({ login = "qwerty", email = "qwerty" }) {
     this.login = login;
     this.email = email;
   };
 
-  Account.prototype.getInfo = function() {
+  Account.prototype.getInfo = function () {
     console.log(`login: ${this.login}, Email: ${this.email}`);
   };
 
@@ -13,12 +13,39 @@ function home1() {
 
   const mango = new Account({
     login: "Mangozedog",
-    email: "mango@dog.woof"
+    email: "mango@dog.woof",
   });
   const poly = new Account({
     login: "Poly",
-    email: "poly@mail.com"
+    email: "poly@mail.com",
   });
   mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
   poly.getInfo(); // Login: Poly, Email: poly@mail.com
 }
+
+// =========================OR============================================================
+// class Account {
+//   constructor(elements) {
+//     this.login = elements.login;
+//     this.email = elements.email;
+//   }
+//   getInfo() {
+//     console.log(`LOGIN: ${this.login}; EMAIL: ${this.email}`);
+//   }
+// }
+
+
+// const mango = new Account({
+//   login: "Mangozedog",
+//   email: "mango@dog.woof",
+// });
+
+// mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+
+// const poly = new Account({
+//   login: "Poly",
+//   email: "poly@mail.com",
+// });
+
+// poly.getInfo(); // Login: Poly, Email: poly@mail.com
+// ====================OR==================================================================
